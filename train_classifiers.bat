429216de1315675862dcac04182370340ba4d445
HCI-Lab MiFCOM PC <mark.colley@yahoo.de>
2025-01-02 21:32:48 +0100

    update

--- train_classifiers.bat
+++ train_classifiers.bat
@@ -7,10 +7,10 @@ REM python preprocess.py
 REM echo "Preprocess complete."
 
 REM Step 2: Train the Decision Tree classifier
-python classifier_dt.py
+REM python classifier_dt.py
 
 REM Step 3: Train the Naive Bayes classifier
-python classifier_nb.py
+REM python classifier_nb.py
 
 REM Step 4: Train the Support Vector Machine classifier
 python classifier_svm.py
